b5d414799bdc264d26f94bb5281f5d65c3480c3a
simrankaur1509 <simrankaur1509@gmail.com>
2024-05-23 17:27:41 +0200

    merging jsons

---
+++ Incentive Scheme Data/generate_table.py
@@ -0,0 +1,25 @@
+import os
+import json
+import pandas as pd
+
+# Directory where JSON files are stored
+json_dir = 'path_to_json_files_directory'
+
+# List to hold data from each JSON file
+data = []
+
+# Iterate over all JSON files and read data
+for json_file in os.listdir(json_dir):
+    if json_file.endswith('.json'):
+        with open(os.path.join(json_dir, json_file)) as f:
+            data.append(json.load(f))
+
+# Create a DataFrame from the data
+df = pd.DataFrame(data)
+
+# Generate HTML table from DataFrame
+html_table = df.to_html(index=False)
+
+# Write HTML table to file
+with open('records_table.html', 'w') as f:
+    f.write(html_table)
