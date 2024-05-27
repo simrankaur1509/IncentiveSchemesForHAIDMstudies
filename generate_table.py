de9871950b01188e587ccc8447f81cf423b16639
simrankaur1509 <simrankaur1509@gmail.com>
2024-05-27 13:00:12 +0200

    Update generate_table.py

--- generate_table.py
+++ generate_table.py
@@ -26,13 +26,19 @@ for json_file in os.listdir(json_dir):
 # # Write HTML table to file
 # with open('index.html', 'w') as f:
 #     f.write(html_table)
-
+            
 def generate_html_table(data):
     html = '<table border="1">'
+    
     # Header
     html += '<thead><tr>'
     for key in data[0].keys():
-        html += f'<th>{key}</th>'
+        if isinstance(data[0][key], dict):
+            nested_keys = data[0][key].keys()
+            for nested_key in nested_keys:
+                html += f'<th>{key}.{nested_key}</th>'
+        else:
+            html += f'<th>{key}</th>'
     html += '</tr></thead>'
     
     # Rows
@@ -41,26 +47,12 @@ def generate_html_table(data):
         html += '<tr>'
         for key, value in record.items():
             if isinstance(value, dict):
-                html += f'<td>{generate_html_table_from_dict(value)}</td>'
+                for nested_key, nested_value in value.items():
+                    html += f'<td>{nested_value}</td>'
             else:
                 html += f'<td>{value}</td>'
         html += '</tr>'
     html += '</tbody>'
-    html += '</table>'
-    return html
-
-def generate_html_table_from_dict(data_dict):
-    html = '<table border="1">'
-    for key, value in data_dict.items():
-        html += '<tr>'
-        html += f'<td>{key}</td>'
-        if isinstance(value, dict):
-            html += f'<td>{generate_html_table_from_dict(value)}</td>'
-        elif isinstance(value, list):
-            html += f'<td>{", ".join(value) if value else ""}</td>'
-        else:
-            html += f'<td>{value}</td>'
-        html += '</tr>'
     html += '</table>'
     return html
 
@@ -70,3 +62,47 @@ html_table = generate_html_table(data)
 # Write HTML table to file
 with open('index.html', 'w') as f:
     f.write(html_table)
+
+# def generate_html_table(data):
+#     html = '<table border="1">'
+#     # Header
+#     html += '<thead><tr>'
+#     for key in data[0].keys():
+#         html += f'<th>{key}</th>'
+#     html += '</tr></thead>'
+    
+#     # Rows
+#     html += '<tbody>'
+#     for record in data:
+#         html += '<tr>'
+#         for key, value in record.items():
+#             if isinstance(value, dict):
+#                 html += f'<td>{generate_html_table_from_dict(value)}</td>'
+#             else:
+#                 html += f'<td>{value}</td>'
+#         html += '</tr>'
+#     html += '</tbody>'
+#     html += '</table>'
+#     return html
+
+# def generate_html_table_from_dict(data_dict):
+#     html = '<table border="1">'
+#     for key, value in data_dict.items():
+#         html += '<tr>'
+#         html += f'<td>{key}</td>'
+#         if isinstance(value, dict):
+#             html += f'<td>{generate_html_table_from_dict(value)}</td>'
+#         elif isinstance(value, list):
+#             html += f'<td>{", ".join(value) if value else ""}</td>'
+#         else:
+#             html += f'<td>{value}</td>'
+#         html += '</tr>'
+#     html += '</table>'
+#     return html
+
+# # Generate HTML table from data
+# html_table = generate_html_table(data)
+
+# # Write HTML table to file
+# with open('index.html', 'w') as f:
+#     f.write(html_table)
