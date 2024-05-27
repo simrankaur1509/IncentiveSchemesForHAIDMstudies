c657f00fdc555606699331e4c11495ceb9b2a424
simrankaur1509 <simrankaur1509@gmail.com>
2024-05-27 13:03:56 +0200

    Update generate_table.py

--- generate_table.py
+++ generate_table.py
@@ -14,55 +14,6 @@ for json_file in os.listdir(json_dir):
         with open(os.path.join(json_dir, json_file)) as f:
             data.append(json.load(f))
 
-# # Normalize nested fields
-# df = pd.json_normalize(data)
-
-# # Create a DataFrame from the data
-# df = pd.DataFrame(data)
-
-# # Generate HTML table from DataFrame
-# html_table = df.to_html(index=False)
-
-# # Write HTML table to file
-# with open('index.html', 'w') as f:
-#     f.write(html_table)
-            
-def generate_html_table(data):
-    html = '<table border="1">'
-    
-    # Header
-    html += '<thead><tr>'
-    for key in data[0].keys():
-        if isinstance(data[0][key], dict):
-            nested_keys = data[0][key].keys()
-            for nested_key in nested_keys:
-                html += f'<th>{key}.{nested_key}</th>'
-        else:
-            html += f'<th>{key}</th>'
-    html += '</tr></thead>'
-    
-    # Rows
-    html += '<tbody>'
-    for record in data:
-        html += '<tr>'
-        for key, value in record.items():
-            if isinstance(value, dict):
-                for nested_key, nested_value in value.items():
-                    html += f'<td>{nested_value}</td>'
-            else:
-                html += f'<td>{value}</td>'
-        html += '</tr>'
-    html += '</tbody>'
-    html += '</table>'
-    return html
-
-# Generate HTML table from data
-html_table = generate_html_table(data)
-
-# Write HTML table to file
-with open('index.html', 'w') as f:
-    f.write(html_table)
-
 # def generate_html_table(data):
 #     html = '<table border="1">'
 #     # Header
@@ -106,3 +57,62 @@ with open('index.html', 'w') as f:
 # # Write HTML table to file
 # with open('index.html', 'w') as f:
 #     f.write(html_table)
+
+def generate_html_table(data):
+    html = '''
+    <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+        <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+    </head>
+    <body>
+    <table id="example" class="display" style="width:100%">
+    '''
+    
+    # Header
+    html += '<thead><tr>'
+    for key in data[0].keys():
+        if isinstance(data[0][key], dict):
+            nested_keys = data[0][key].keys()
+            for nested_key in nested_keys:
+                html += f'<th>{key}.{nested_key}</th>'
+        else:
+            html += f'<th>{key}</th>'
+    html += '</tr></thead>'
+    
+    # Rows
+    html += '<tbody>'
+    for record in data:
+        html += '<tr>'
+        for key, value in record.items():
+            if isinstance(value, dict):
+                for nested_key, nested_value in value.items():
+                    html += f'<td>{nested_value}</td>'
+            else:
+                html += f'<td>{value}</td>'
+        html += '</tr>'
+    html += '</tbody>'
+    html += '''
+    </table>
+    <script>
+    $(document).ready(function() {
+        $('#example').DataTable({
+            "columnDefs": [
+                { "type": "num", "targets": 3 },  // Adjust the index for "year"
+                { "type": "string", "targets": 2 }  // Adjust the index for "venue"
+            ]
+        });
+    });
+    </script>
+    </body>
+    </html>
+    '''
+    return html
+
+# Generate HTML table from data
+html_table = generate_html_table(data)
+
+# Write HTML table to file
+with open('index.html', 'w') as f:
+    f.write(html_table)
