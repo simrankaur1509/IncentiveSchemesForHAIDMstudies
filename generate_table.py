41539026ac26bfa9c4eb5f287d31c2b8c87ef450
simrankaur1509 <simrankaur1509@gmail.com>
2024-05-27 13:20:28 +0200

    Update generate_table.py

--- generate_table.py
+++ generate_table.py
@@ -9,82 +9,116 @@ data = []
 
 # Iterate over all JSON files and read data
 for json_file in os.listdir(json_dir):
-  if json_file.endswith('.json'):
-    with open(os.path.join(json_dir, json_file)) as f:
-      data.append(json.load(f))
+    if json_file.endswith('.json'):
+        with open(os.path.join(json_dir, json_file)) as f:
+            data.append(json.load(f))
+
+def extract_headers(data):
+    headers = {}
+
+    def add_to_headers(d, parent_key=''):
+        for k, v in d.items():
+            full_key = f"{parent_key}.{k}" if parent_key else k
+            if isinstance(v, dict):
+                if parent_key not in headers:
+                    headers[parent_key] = {}
+                headers[parent_key][k] = {}
+                add_to_headers(v, full_key)
+            else:
+                if parent_key not in headers:
+                    headers[parent_key] = {}
+                headers[parent_key][k] = None
+    
+    add_to_headers(data[0])
+    return headers
 
 def generate_html_table(data):
-  html = '''
-  <html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
-    <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
-  </head>
-  <body>
-  <table id="example" class="display" style="width:100%">
-  '''
-
-  # Header Row
-  html += '<thead><tr>'
-  headers = []
-
-  # Collect top-level headers
-  for key in data[0].keys():
-    headers.append(key)
-
-  for header in headers:
-    html += f'<th>{header}</th>'
-
-  html += '</tr></thead>'
-
-  # Rows
-  html += '<tbody>'
-  for record in data:
+    headers = extract_headers(data)
+    
+    html = '''
+    <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+        <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+    </head>
+    <body>
+    <table id="example" class="display" style="width:100%">
+    '''
+    
+    # Generate header rows
+    html += '<thead>'
     html += '<tr>'
-    for key in record.keys():
-      # Check for nested data
-      if isinstance(record[key], dict):
-        html += f'<td colspan="{len(record[key].keys())}">'  # Span for nested cells
-        # Generate nested table for each record
-        html += f'''
-          <table style="width:100%">
-            <tr>'''
-
-        for nested_key in record[key].keys():
-          html += f'<th>{nested_key}</th>'
-
-        html += '</tr><tr>'
-        for nested_value in record[key].values():
-          html += f'<td>{nested_value}</td>'
-
-        html += '</tr></table>'
-        html += '</td>'
-      else:
-        html += f'<td>{record[key]}</td>'
+    for key, subkeys in headers.items():
+        if subkeys:
+            for subkey, nested_subkeys in subkeys.items():
+                if nested_subkeys:
+                    col_span = len(nested_subkeys)
+                    html += f'<th colspan="{col_span}">{key}.{subkey}</th>'
+                else:
+                    html += f'<th>{key}.{subkey}</th>'
+        else:
+            html += f'<th>{key}</th>'
     html += '</tr>'
-  html += '</tbody>'
-  html += '''
-  </table>
-  <script>
-  $(document).ready(function() {
-    $('#example').DataTable({
-      "columnDefs": [
-        { "orderable": true, "targets": 0 },  # Year (assuming first column)
-        { "orderable": true, "targets": 1 },  # Venue (assuming second column)
-        { "orderable": false, "targets": "_all" }
-      ]
+    
+    html += '<tr>'
+    for key, subkeys in headers.items():
+        if subkeys:
+            for subkey, nested_subkeys in subkeys.items():
+                if nested_subkeys:
+                    for nested_subkey in nested_subkeys.keys():
+                        html += f'<th>{nested_subkey}</th>'
+                else:
+                    html += f'<th>{subkey}</th>'
+        else:
+            html += f'<th>{key}</th>'
+    html += '</tr>'
+    html += '</thead>'
+    
+    # Generate table rows
+    html += '<tbody>'
+    for record in data:
+        html += '<tr>'
+        for key, subkeys in headers.items():
+            if subkeys:
+                for subkey, nested_subkeys in subkeys.items():
+                    if nested_subkeys:
+                        for nested_subkey in nested_subkeys.keys():
+                            nested_record = get_nested_record(record, [key, subkey])
+                            html += f'<td>{nested_record.get(nested_subkey, "")}</td>'
+                    else:
+                        nested_record = record.get(key, {})
+                        html += f'<td>{nested_record.get(subkey, "")}</td>'
+            else:
+                html += f'<td>{record.get(key, "")}</td>'
+        html += '</tr>'
+    html += '</tbody>'
+    
+    html += '''
+    </table>
+    <script>
+    $(document).ready(function() {
+        $('#example').DataTable({
+            "columnDefs": [
+                { "orderable": true, "targets": [2, 3] },  // "year" and "venue" columns (adjust indices if necessary)
+                { "orderable": false, "targets": "_all" }
+            ]
+        });
     });
-  });
-  </script>
-  </body>
-  </html>
-  '''
-  return html
+    </script>
+    </body>
+    </html>
+    '''
+    return html
+
+def get_nested_record(record, keys):
+    for key in keys:
+        record = record.get(key, {})
+    return record
 
 # Generate HTML table from data
 html_table = generate_html_table(data)
 
 # Write HTML table to file
 with open('index.html', 'w') as f:
-  f.write(html_table)
+    f.write(html_table)
