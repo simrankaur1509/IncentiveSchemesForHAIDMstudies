faa165f0aa3e4fa51568b0bd4ab1d5d2c59f9e2f
simrankaur1509 <simrankaur1509@gmail.com>
2024-06-30 21:28:43 +0200

    Clean-up

--- generate_table.py
+++ generate_table.py
@@ -14,61 +14,40 @@ for json_file in os.listdir(json_dir):
             data.append(json.load(f))
 
 def generate_html_table(data):
-    html = '''
-    <html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
-        <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
-        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
-    </head>
-    <body>
-    <table id="example" class="display" style="width:100%">
-    '''
-
+    html = '<table border="1">'
     # Header
     html += '<thead><tr>'
-    headers = []
-
-    # Collect headers
     for key in data[0].keys():
-        if isinstance(data[0][key], dict):
-            for nested_key in data[0][key].keys():
-                headers.append(f'{key}.{nested_key}')
-        else:
-            headers.append(key)
-
-    for header in headers:
-        html += f'<th>{header}</th>'
-
+        html += f'<th>{key}</th>'
     html += '</tr></thead>'
-
+    
     # Rows
     html += '<tbody>'
     for record in data:
         html += '<tr>'
-        for key in record.keys():
-            if isinstance(record[key], dict):
-                for nested_key in record[key].keys():
-                    html += f'<td>{record[key][nested_key]}</td>'
+        for key, value in record.items():
+            if isinstance(value, dict):
+                html += f'<td>{generate_html_table_from_dict(value)}</td>'
             else:
-                html += f'<td>{record[key]}</td>'
+                html += f'<td>{value}</td>'
         html += '</tr>'
     html += '</tbody>'
-    html += '''
-    </table>
-    <script>
-    $(document).ready(function() {
-        $('#example').DataTable({
-            "columnDefs": [
-                { "type": "num", "targets": 3 },  // Adjust the index for "year"
-                { "type": "string", "targets": 2 }  // Adjust the index for "venue"
-            ]
-        });
-    });
-    </script>
-    </body>
-    </html>
-    '''
+    html += '</table>'
+    return html
+
+def generate_html_table_from_dict(data_dict):
+    html = '<table border="1">'
+    for key, value in data_dict.items():
+        html += '<tr>'
+        html += f'<td>{key}</td>'
+        if isinstance(value, dict):
+            html += f'<td>{generate_html_table_from_dict(value)}</td>'
+        elif isinstance(value, list):
+            html += f'<td>{", ".join(value) if value else ""}</td>'
+        else:
+            html += f'<td>{value}</td>'
+        html += '</tr>'
+    html += '</table>'
     return html
 
 # Generate HTML table from data
@@ -77,3 +56,62 @@ html_table = generate_html_table(data)
 # Write HTML table to file
 with open('index.html', 'w') as f:
     f.write(html_table)
+
+# def generate_html_table(data):
+#     html = '''
+#     <html>
+#     <head>
+#         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+#         <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+#         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+#     </head>
+#     <body>
+#     <table id="example" class="display" style="width:100%">
+#     '''
+    
+#     # Header
+#     html += '<thead><tr>'
+#     for key in data[0].keys():
+#         if isinstance(data[0][key], dict):
+#             nested_keys = data[0][key].keys()
+#             for nested_key in nested_keys:
+#                 html += f'<th>{key}.{nested_key}</th>'
+#         else:
+#             html += f'<th>{key}</th>'
+#     html += '</tr></thead>'
+    
+#     # Rows
+#     html += '<tbody>'
+#     for record in data:
+#         html += '<tr>'
+#         for key, value in record.items():
+#             if isinstance(value, dict):
+#                 for nested_key, nested_value in value.items():
+#                     html += f'<td>{nested_value}</td>'
+#             else:
+#                 html += f'<td>{value}</td>'
+#         html += '</tr>'
+#     html += '</tbody>'
+#     html += '''
+#     </table>
+#     <script>
+#     $(document).ready(function() {
+#         $('#example').DataTable({
+#             "columnDefs": [
+#                 { "type": "num", "targets": 3 },  // Adjust the index for "year"
+#                 { "type": "string", "targets": 2 }  // Adjust the index for "venue"
+#             ]
+#         });
+#     });
+#     </script>
+#     </body>
+#     </html>
+#     '''
+#     return html
+
+# # Generate HTML table from data
+# html_table = generate_html_table(data)
+
+# # Write HTML table to file
+# with open('index.html', 'w') as f:
+#     f.write(html_table)
